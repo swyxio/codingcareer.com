faa812f5096cd8d8805c2bfe80ced93bfd9f6e76
swyx <shawnthe1@gmail.com>
2020-07-02 05:38:25 +0800

    favicons and ogimage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,30 @@
   import Footer from "../components/Footer.svelte";
 </script>
 
+<svelte:head>
+  <title>The Coding Career Handbook</title>
+  <meta property="og:type" content="books.book" />
+  <meta property="og:title" content="The Coding Career Handbook" />
+  <meta property="og:site_name" content="Learn in Public" />
+  <meta property="og:url" content="https://www.learninpublic.org" />
+  <meta
+    property="og:description"
+    content="450+ pages of the best advice I have ever written, with Career
+    Guides, Principles, Strategies, and Tactics for the Junior-to-Senior
+    developer." />
+  <meta
+    property="og:image"
+    content="https://www.learninpublic.org/codingcareer/ogimage.png" />
+  <meta property="books:rating:value" content="5" />
+  <meta property="books:rating:scale" content="5" />
+  <meta property="books:author" content="https://swyx.io" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:label1" content="Followers" />
+  <meta name="twitter:data1" content="1900+" />
+  <meta name="twitter:label2" content="Reviews" />
+  <meta name="twitter:data2" content="⭐⭐⭐⭐⭐" />
+</svelte:head>
+
 <div class="flex flex-col min-h-screen font-sans text-gray-900">
   <main>
     <div class="relative bg-gray-100 overflow-hidden">
