60789ef7e18da55b3902c65d2dfd151b1bc0c078
swyx <shawnthe1@gmail.com>
2020-07-03 08:35:18 +0800

    tweak announcebar

--- src/components/AnnounceBar.svelte
+++ src/components/AnnounceBar.svelte
@@ -1,12 +1,11 @@
 <div
   class="font-medium absolute w-full text-md text-center text-black
   bg-yellow-500 p-2">
-  🎉 Celebrating our Launch! 25% of Book Sales will go to support
-  <a
+  🎉 Celebrating our Launch! 25% of Book Sales will go to <a
     class="pl-1 underline"
-    href="https://www.freecodecamp.org/"
+    href="https://www.freecodecamp.org/news/freecodecamp-servers-update-october-2019/"
     target="_blank"
     rel="noreferrer">
-    FreeCodeCamp.
+  support FreeCodeCamp.
   </a>
 </div>
