512ee42b70e3fc6539e82c826a54fbc756103674
sw-yx <shawnthe1@gmail.com>
2020-10-25 06:59:02 +0800

    push it live on the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,27 @@
+const production = !process.env.ROLLUP_WATCH;
 module.exports = {
-  purge: [],
+  purge:  {
+    // mode: 'all', // TODO: purge typography styles
+    // options: {
+    //   whitelist: ['h1', 'h2', 'h3', 'p', 'blockquote', 'strong' /* etc. */],
+    // },
+    content: [
+      "./src/**/*.svelte",
+      // may also want to include base index.html
+    ],
+    defaultExtractor: content => {
+      // Capture as liberally as possible, including things like `h-(screen-1.5)`
+      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+      const broadMatchesWithoutTrailingSlash = broadMatches.map(match => _.trimEnd(match, '\\'))
+      // // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+      // const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+      const matches = broadMatches
+        .concat(broadMatchesWithoutTrailingSlash)
+      // .concat([...content.matchAll(/(?:class:)*([\w\d-\/:%.]+)/gm)].map(([_match, group, ..._rest]) => group))
+      return matches
+    },
+    enabled: production // disable purge in dev
+  },
   theme: {
     extend: {},
   },
