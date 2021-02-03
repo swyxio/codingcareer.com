6da070fc9e258118a7a027bd4a47c812a3b37cda
Ron Au <git@roncya.com>
2021-02-03 23:12:10 +1100

    Restore Tailwind 1 colours - en-US: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,43 @@ module.exports = {
     enabled: production // disable purge in dev
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+        yellow: {
+          100: '#fffff0',
+          200: '#fefcbf',
+          300: '#faf089',
+          400: '#f6e05e',
+          500: '#ecc94b',
+          600: '#d69e2e',
+          700: '#b7791f',
+          800: '#975a16',
+          900: '#744210'
+        }
+      },
+    },
   },
   variants: {},
   plugins: [],
