5752248ab0ae79a9af5ed9717a38cfc55c436992
Ron Au <git@roncya.com>
2021-02-06 17:39:50 +1100

    Fix button whitespace in FAQ

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,7 +6,7 @@ export let cta = false;
 
 const classes = {
   base: 'block font-semibold leading-6 text-center text-black border-black rounded-lg shadow-md sm:w-1/3 font-display',
-  small: ' px-3 py-2 m-0 mx-auto mt-2',
+  small: ' px-3 py-2 m-0 mx-auto mt-10',
   large: ' w-full px-6 py-4 my-5 mr-5 text-xl',
   cta: ' bg-yellow-500 hover:bg-yellow-600 border-2',
   plain: ' bg-transparent hover:bg-yellow-500'
