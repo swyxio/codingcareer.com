6e309bc505978e466b3d61d210cb07b7fdd16e0d
Ron Au <git@roncya.com>
2021-03-01 18:47:45 +1100

    Restyle Button component - border removed - rename 'search' to 'target' - remove hardcoded #buy target - brighten hover state - add active states

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,19 +1,19 @@
 <script>
-export let text = 'Buy Now';
-export let search = '';
-export let large = false;
-export let cta = false;
-
-const classes = {
-  base: 'block font-semibold leading-6 text-center text-black border-black rounded-lg shadow-md sm:w-1/3 font-display',
-  small: ' px-3 py-2 m-0 mx-auto mt-10',
-  large: ' w-full px-6 py-4 my-5 mr-5 text-xl',
-  cta: ' bg-yellow-500 hover:bg-yellow-600 border-2',
-  plain: ' bg-transparent hover:bg-yellow-500'
-}
+  export let text = 'Buy Now';
+  export let target = '';
+  export let large = false;
+  export let cta = false;
+  
+  const classes = {
+    base: 'block font-semibold leading-6 text-center text-black rounded-lg shadow-md sm:w-1/3 font-display hover:bg-yellow-450 transform active:scale-95 active:shadow-0',
+    small: ' px-3 py-3 m-0 mx-auto mt-10',
+    large: ' w-full px-6 py-5 my-5 mr-5 text-xl',
+    cta: ' bg-yellow-500 focus:bg-yellow-450',
+    plain: ' bg-transparent'
+  }
 </script>
-
+  
 <a class={classes.base + (large ? classes.large : classes.small) + (cta ? classes.cta : classes.plain)}
-  href={`${search}#buy`}>
+  href={target}>
   {text}
 </a>
